783d2f2e343745080596ee30e418faa73690bcf9
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-28 02:00:01 +0630

    connecting to the database

--- server/index.js
+++ server/index.js
@@ -6,5 +6,15 @@ import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlendcoded({ limit: "30mb", extended: true}));
-app.use(bodyParser.cors({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://kurosz:asd123@nodetuts.hhvdf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((err) => console.log(err.message));
+
+// mongoose.set('useFindAndModify', false); (no longer necessary)
+
