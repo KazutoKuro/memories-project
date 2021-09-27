93fb47e9024212b5fa9d1edb488920072f3d34c4
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-28 01:38:15 +0630

    index.js setup

--- server/index.js
+++ server/index.js
@@ -2,3 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlendcoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.cors({ limit: "30mb", extended: true}));
