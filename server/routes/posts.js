66db3b35bdaf65eb92740c0b4f46b5422a221f31
KauztoKuro <ikkikurogane70@gmail.com>
2021-10-01 02:56:23 +0630

    Controllers

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getPosts } from '../controllers/posts';
+import { getPosts, createPosts } from '../controllers/posts';
 
 
 const router = express.Router();
 
 // localhost:5000/posts
 router.get('/', getPosts);
+router.post('/', createPosts);
 
 export default router;
