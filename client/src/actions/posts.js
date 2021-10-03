562cad854be453c1fe04579f505cbdf09adcb38c
KauztoKuro <ikkikurogane70@gmail.com>
2021-10-03 23:34:49 +0630

    redux

---
+++ client/src/actions/posts.js
@@ -0,0 +1,16 @@
+import * as api from '../api';
+
+// Action Creators
+export const getPosts = () => async (disptach) => {
+    try {
+        const { data } = await api.fetchPosts();
+
+        dispatchEvent({ type: 'FETCH_ALL', payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    // const action = { type: 'FETCH_ALL', payload: []}
+
+    // disptach (action);
+}
