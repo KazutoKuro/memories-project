2f0ee55a69ed74f8f3f076ce4597b7b14a39c683
KauztoKuro <ikkikurogane70@gmail.com>
2021-10-07 21:10:43 +0630

    changing database at server side

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://kurosz:asd123@nodetuts.hhvdf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://kurosz:asd123@nodetuts.hhvdf.mongodb.net/memories-project?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
