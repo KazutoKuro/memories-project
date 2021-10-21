476a08ff740f0e297782220ee28f9f3c9ecc98eb
KauztoKuro <ikkikurogane70@gmail.com>
2021-10-22 02:26:00 +0630

    Auth middleware

---
+++ Part_3/server/middleware/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+//wnats to like a post
+// click the like button => auth middleware(next) => like controller ...
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.spilt(" ")[1];
+        const isCustomAuth = token.lenght < 500;
+
+        let decodedData;
+
+        if (token && isCustomAuth){
+            decodedData = jwt.verify(token, 'test');
+
+            req.userId = decodedData?.id;
+        } else {
+            decodedData = jwt.decode(token);
+
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+    }   
+}
+
+export default auth;
