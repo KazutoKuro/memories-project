f9dfe70e9ea4b43361457c4e810615e71c3227dc
KauztoKuro <ikkikurogane70@gmail.com>
2021-10-21 22:33:14 +0630

    JWT Auth

--- Part_3/client/src/components/Auth/Auth.js
+++ Part_3/client/src/components/Auth/Auth.js
@@ -8,22 +8,33 @@ import Icon from './icon';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
 import Input from './Input';
+import { signin, signup } from '../../actions/auth';
 
+const initialState = { firstName: '', lastName: '',email: '', password: '', confirmPassword: ''};
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
+    const [formData, setFormData] = useState(initialState);
     const dispatch = useDispatch();
     const history = useHistory();
 
-    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
-    const handleSubmit = () => {
 
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if(isSignup){
+            dispatch(signup(formData, history))
+        } else {
+            dispatch(signin(formData, history))
+        }
     };
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     };
 
     const switchMode = () =>{
@@ -60,7 +71,7 @@ const Auth = () => {
                             isSignup && (
                                 <>
                                     <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-                                    <Input name="firstName" label="First Name" handleChange={handleChange} half />
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />
                                 </>
                             )
                         }
