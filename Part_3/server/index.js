c5b82dd11d37a32323c25b93a0d1303f7cf7674f
KauztoKuro <ikkikurogane70@gmail.com>
2021-10-22 01:42:23 +0630

    JWT Auth Backend

--- Part_3/server/index.js
+++ Part_3/server/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://kurosz:asd123@nodetuts.hhvdf.mongodb.net/memories-project';
 const PORT = process.env.PORT|| 5000;
