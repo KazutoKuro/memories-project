40d0016d16463ccb37c8130de4b124a55971d8dc
KauztoKuro <ikkikurogane70@gmail.com>
2021-10-24 11:42:46 +0630

    Token Expiry

--- Part_3/client/src/components/Navbar/Navbar.js
+++ Part_3/client/src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 import useStyles from './styles';
 import memories from '../../images/memories.png';
 
@@ -26,6 +27,11 @@ const Navbar = () => {
         const token = user?.token;
 
         //JWT
+        if(token) {
+            const decodedToken = decode(token);
+            
+            if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
 
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]);
