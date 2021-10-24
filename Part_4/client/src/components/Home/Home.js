b618a5019c7fe2378e253d1f0a68fc3556eb7afb
KauztoKuro <ikkikurogane70@gmail.com>
2021-10-24 20:57:35 +0630

    Pagination

--- Part_4/client/src/components/Home/Home.js
+++ Part_4/client/src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from '../../actions/posts';
+import Pagination from '../pagination';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import { mergeClasses } from '@material-ui/styles';
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -23,6 +25,9 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper  elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
