a6c1ce65a181ef945aac42da2da6a0639e2c8c99
KauztoKuro <ikkikurogane70@gmail.com>
2021-11-01 21:41:18 +0630

    Loading

--- Part_4/client/src/actions/posts.js
+++ Part_4/client/src/actions/posts.js
@@ -1,11 +1,13 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, START_LOADING, END_LOADING, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
 export const getPosts = (page) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING});
     const { data } = await api.fetchPosts(page);
 
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: END_LOADING});
   } catch (error) {
     console.log(error);
   }
@@ -13,9 +15,11 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING});
     const { data: {data} } = await api.fetchPostsBySearch(searchQuery);
     
     dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    dispatch({ type: END_LOADING});
   } catch (error) {
     console.log(error);
   }
@@ -23,6 +27,7 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING});
     const { data } = await api.createPost(post);
 
     dispatch({ type: CREATE, payload: data });
