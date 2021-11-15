f07aa2a5177a872f30545a6beddca5d14d20e830
KauztoKuro <ikkikurogane70@gmail.com>
2021-11-16 01:16:41 +0630

    backend comment

--- Part_5/client/src/actions/posts.js
+++ Part_5/client/src/actions/posts.js
@@ -1,4 +1,4 @@
-import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE, COMMENT } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
 export const getPost = (id) => async (dispatch) => {
@@ -74,9 +74,13 @@ export const likePost = (id) => async (dispatch) => {
 
 export const commentPost = (value, id) => async (dispatch) => {
   try {
-    await api.comment(value, id);
+    const { data } = await api.comment(value, id);
+
+    dispatch({ type: COMMENT, payload: data});
+
+    return data.comments;
   } catch (error) {
-    
+    console.log(error);
   }
 }
 
--- Part_5/client/src/components/PostDetails/CommentSection.jsx
+++ Part_5/client/src/components/PostDetails/CommentSection.jsx
@@ -7,9 +7,9 @@ import { commentPost } from '../../actions/posts';
 
 const CommectSection = ({ post }) => {
     const classes = useStyles();
-    const [comments, setComments] = useState([1,2,3,4]);
+    const [comments, setComments] = useState(post?.comments);
     const [comment, setComment] = useState('');
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem('profile'));
     const dispatch = useDispatch();
 
     const handleClick = () => {
@@ -25,25 +25,27 @@ const CommectSection = ({ post }) => {
                     <Typography gutterBottom variant="h6">Comments</Typography>
                     {comments.map((c, i) => (
                         <Typography key={i} gutterBottom variant="subtitle1">
-                            Comment {i}
+                            {c}
                         </Typography>
                     ))}
                 </div>
-                <div style={{ width: '70%'}}>
-                    <Typography gutterBottom variant="h6">Write a Comment</Typography>
-                    <TextField 
-                        fullWidth
-                        rows={4}
-                        variant="outlined"
-                        label="Comment"
-                        multiline
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                    />
-                    <Button style={{ marginTop: '10px'}} fullWidth disabled={!comment} variant="contained" color="primary" onClick={handleClick}>
-                        Comment
-                    </Button>
-                </div>
+                {user?.result?.name && (
+                    <div style={{ width: '70%'}}>
+                        <Typography gutterBottom variant="h6">Write a Comment</Typography>
+                        <TextField 
+                            fullWidth
+                            rows={4}
+                            variant="outlined"
+                            label="Comment"
+                            multiline
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                        />
+                        <Button style={{ marginTop: '10px'}} fullWidth disabled={!comment} variant="contained" color="primary" onClick={handleClick}>
+                            Comment
+                        </Button>
+                    </div>
+                )}
             </div>
         </div>
     );
