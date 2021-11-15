8c291677cf5a26f8e08be31eabfb5d5b909a16f6
KauztoKuro <ikkikurogane70@gmail.com>
2021-11-16 00:44:15 +0630

    Comments

---
+++ Part_5/client/src/components/PostDetails/CommentSection.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from 'react';
+import { Typography, TextField, Button } from '@material-ui/core';
+import { useDispatch} from 'react-redux';
+
+import useStyles from './styles';
+import { mergeClasses } from '@material-ui/styles';
+
+const CommectSection = ({ post }) => {
+    console.log(post);
+    const classes = useStyles();
+    const [comments, setComments] = useState([1,2,3,4]);
+    const [comment, setComment] = useState('');
+
+    const handleClick = () => {
+
+    };
+
+    return(
+        <div>
+            <div className={classes.commentsOuterContainer}>
+                <div className={classes.commentsInnerContainer}>
+                    <Typography gutterBottom variant="h6">Comments</Typography>
+                    {comments.map((c, i) => (
+                        <Typography key={i} gutterBottom variant="subtitle1">
+                            Comment {i}
+                        </Typography>
+                    ))}
+                </div>
+                <div style={{ width: '70%'}}>
+                    <Typography gutterBottom variant="h6">Write a Comment</Typography>
+                    <TextField 
+                        fullWidth
+                        rows={4}
+                        variant="outlined"
+                        label="Comment"
+                        multiline
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                    />
+                    <Button style={{ marginTop: '10px'}} fullWidth disabled={!comment} variant="contained" color="primary" onClick={handleClick}>
+                        Comment
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CommectSection;
